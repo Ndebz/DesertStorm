a2fc8eecc477de84d36d028d63f75522d17ab48d
Ndebz <nqobilendebele@msn.com>
2014-11-20 23:30:25 +0000

    Adding Body model

---
+++ Models/Body.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DesertStorm.Models
+{
+    public class Body
+    {
+        public int BodyID { get; set; }
+        public string BodyType { get; set; }
+    }
+}
